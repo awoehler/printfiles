f6ffbac6a861907083bbccae1e79b6bbfc97b1ed
Aaron Woehler <awoehler@woehler.us>
2016-11-04 09:20:41 -0800

    Fixed multi-page printing.

---
+++ printfiles/Document.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Drawing.Printing;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace printfiles
+{
+    public class Document
+    {
+        private List<Image> images;
+        private Image currentImagePage;
+        public string filename { get; set; }
+        public Document(string filename)
+        {
+            this.filename = filename;
+            images = new List<Image>();
+            Bitmap bitmap = (Bitmap)Image.FromFile(filename);
+            int count = bitmap.GetFrameCount(FrameDimension.Page);
+            for (int idx = 0; idx < count; idx++)
+            {
+                // save each frame to a bytestream
+                bitmap.SelectActiveFrame(FrameDimension.Page, idx);
+                MemoryStream byteStream = new MemoryStream();
+                bitmap.Save(byteStream, ImageFormat.Tiff);
+
+                // and then create a new Image from it
+                images.Add(Image.FromStream(byteStream));
+            }
+        }
+
+        public void print()
+        {
+            foreach (Image image in images)
+            {
+                currentImagePage = image;
+                PrintDocument pd = new PrintDocument();
+                pd.DefaultPageSettings.Color = false;
+                pd.PrintPage += PrintPage;
+                pd.Print();
+            }
+        }
+
+        private void PrintPage(object o, PrintPageEventArgs e)
+        {
+
+            Rectangle m = e.PageBounds;
+
+            if ((double)currentImagePage.Width / (double)currentImagePage.Height > (double)m.Width / (double)m.Height) // image is wider
+            {
+                e.PageSettings.Landscape = true;
+                m.Height = (int)((double)currentImagePage.Height / (double)currentImagePage.Width * (double)m.Width) - 2;
+                e.PageBounds.Inflate(1098, 848);
+
+            }
+            else
+            {
+                e.PageSettings.Landscape = false;
+                m.Width = (int)((double)currentImagePage.Width / (double)currentImagePage.Height * (double)m.Height) - 2;
+                e.PageBounds.Inflate(848, 1098);
+            }
+
+            e.PageBounds.Offset(1, 1);
+            e.Graphics.DrawImage(currentImagePage, m);
+        }
+    }
+}
--- printfiles/Program.cs
+++ printfiles/Program.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//using System.Printing;
 using System.IO;
-using System.Drawing.Printing;
-using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace printfiles
 {
@@ -15,64 +7,12 @@ namespace printfiles
     {
         static void Main(string[] args)
         {
-            print();
-         }
-        static void print()
-        {
-            PrintDocument pd = new PrintDocument();
-            pd.DefaultPageSettings.Color = false;
-            pd.PrintPage += PrintPage;
-            pd.Print();
-        }
-
-        static List<Image> GetAllPages(string file)
-        {
-            List<Image> images = new List<Image>();
-            Bitmap bitmap = (Bitmap)Image.FromFile(file);
-            int count = bitmap.GetFrameCount(FrameDimension.Page);
-            for (int idx = 0; idx < count; idx++)
-            {
-                // save each frame to a bytestream
-                bitmap.SelectActiveFrame(FrameDimension.Page, idx);
-                MemoryStream byteStream = new MemoryStream();
-                bitmap.Save(byteStream, ImageFormat.Tiff);
-
-                // and then create a new Image from it
-                images.Add(Image.FromStream(byteStream));
-            }
-            return images;
-        }
-
-        static void PrintPage(object o, PrintPageEventArgs e)
-        {
-            String[] allfiles = System.IO.Directory.GetFiles( Directory.GetCurrentDirectory(), "*.tif", System.IO.SearchOption.TopDirectoryOnly );
-
+            String[] allfiles = System.IO.Directory.GetFiles(Directory.GetCurrentDirectory(), "*.tif", System.IO.SearchOption.TopDirectoryOnly);
             foreach (var file in allfiles)
             {
-                List<Image> pages = GetAllPages(file);
-                foreach( Image page in pages )
-                {
-                    Rectangle m = e.PageBounds;
-
-                    if ((double)page.Width / (double)page.Height > (double)m.Width / (double)m.Height) // image is wider
-                    {
-                        e.PageSettings.Landscape = true;
-                        m.Height = (int)((double)page.Height / (double)page.Width * (double)m.Width) - 2;
-                        e.PageBounds.Inflate(1098, 848);
-
-                    }
-                    else
-                    {
-                        e.PageSettings.Landscape = false;
-                        m.Width = (int)((double)page.Width / (double)page.Height * (double)m.Height) - 2;
-                        e.PageBounds.Inflate(848, 1098);
-                    }
-
-                    e.PageBounds.Offset(1, 1);
-                    e.Graphics.DrawImage(page, m);
-                }
+                printfiles.Document doc = new printfiles.Document(file);
+                doc.print();
             }
-        }
-
+         }
     }
 }
